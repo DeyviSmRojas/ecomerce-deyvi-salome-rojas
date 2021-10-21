f266a661754a3db49af2b74760423a1b7856a1ff
DeyviSR <salomerojasdeyvi@gmail.com>
2021-10-20 22:01:47 -0500

    avance

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import './App.css';
 import NavBar from './components/NavBar';
+import Navbar from './components/navegacion/Navbar';
+import Inicio from './components/paginas/Inicio';
+import Nosotros from './components/paginas/Nosotros';
+import Productos from './components/paginas/Productos';
 
 function App() {
   return (
-    <div className="App" style={{backgroundColor:'red'}}>
+    <div className="App">
       
-      <NavBar />
+      <Router>
+        <NavBar />
+      </Router>
 
        
         
---
+++ src/components/paginas/Inicio.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Inicio = () => {
+    return (
+        <div>
+            Pagina de Inicio
+        </div>
+    )
+}
+
+export default Inicio
