b522df337407f9d6c340c619b0857ff697d1e328
DeyviSR <salomerojasdeyvi@gmail.com>
2021-10-20 22:06:11 -0500

    navbae

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import './App.css';
-import NavBar from './components/NavBar';
 import Navbar from './components/navegacion/Navbar';
 import Inicio from './components/paginas/Inicio';
 import Nosotros from './components/paginas/Nosotros';
